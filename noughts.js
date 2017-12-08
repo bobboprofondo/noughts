e2b06b160d1611b1628617d724b56818eb0def9e
bobboprofondo <18679783+bobboprofondo@users.noreply.github.com>
2017-12-08 14:55:05 +0000

    Create noughts.js

---
+++ noughts.js
@@ -0,0 +1,27 @@
+//Test JS working out of noughts and crosses ML algorithm
+var funks = require('./funks');
+var prompt = require('prompt');
+
+var state = funks.createArray(9);
+
+prompt.start();
+
+prompt.get(['nextMove'], function(err, result) {
+  if (err) {return onErr(err); }
+  console.log('nextMove is ' + result.nextMove);
+});
+
+
+//Initialise game state
+for (i = 0; i < 9; i++) {
+  state[i] = 0;
+};
+
+//displayState(state);
+state[0] = 2;
+state[1] = 0;
+state[2] = 1;
+state[4] = 2;
+
+funks.displayState(state);
+//nextMove = funks.getUserInput();
